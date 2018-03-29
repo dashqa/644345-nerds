7261558306719f67e99a3e9ce3b27e08fa05a2af
Зайцева Дарья <errorka792@gmail.com>
2018-03-29 16:51:09 +0300

    Добавила JS

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ var btn = document.querySelector(".btn__feedback");
 var popup = document.querySelector(".feedback__modal");
 var close = popup.querySelector(".close_window");
 
-var form = popup.querySelector("form");
+var form = popup.querySelector(".feedback__form");
 var name = popup.querySelector("[name=contact_name]");
-var email = popup.querySelector("[contact_email]");
+var email = popup.querySelector("[name=contact_email]");
+var msg = popup.querySelector("[name=contact_msg]")
 
 btn.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -15,6 +16,16 @@ btn.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+feedback__form.addEventListener("submit", function (evt) {
+  if (!contact_name.value || !contact_email.value || !contact_msg) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -22,6 +33,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
